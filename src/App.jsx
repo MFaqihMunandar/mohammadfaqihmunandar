a75d1ce9eca5b12c8da1b38adb8232849cedcd01
MfaqihMunandar <faqihmunandar479@gmail.com>
2026-08-26 09:54:59 +0700

    ganti nama logo from um.f.m become pm.f.m

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ function App() {
       <nav className="navbar">
         <div className="nav-inner">
           <h2 className="nav-logo" onClick={() => scrollToSection('home')}>
-            uM.F.M<span style={{ color: '#2563eb' }}>.</span>
+            PM.F.M<span style={{ color: '#2563eb' }}>.</span>
           </h2>
 
           <div style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
