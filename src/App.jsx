522116ebb27ecfcd32801987c4712461048aef0a
MfaqihMunandar <faqihmunandar479@gmail.com>
2026-08-26 10:20:09 +0700

    update lagi untuk test git push jalan

--- src/App.jsx
+++ src/App.jsx
@@ -129,7 +129,7 @@ function App() {
       <nav className="navbar">
         <div className="nav-inner">
           <h2 className="nav-logo" onClick={() => scrollToSection('home')}>
-            PM.F.M<span style={{ color: '#2563eb' }}>.</span>
+            please run M.F.M<span style={{ color: '#2563eb' }}>.</span>
           </h2>
 
           <div style={{ display: 'flex', alignItems: 'center', gap: '12px' }}>
